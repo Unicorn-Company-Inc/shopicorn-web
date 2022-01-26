672f79cc316d9e0e5b384c5a20ee4a55f739eb5b
Nico Franke <nico.franke01@gmail.com>
2022-01-26 17:49:05 +0100

    :tada: Initial commit

---
+++ src/layouts/default.vue
@@ -0,0 +1,18 @@
+<template>
+  <q-layout view="hHh lpR fFf">
+    <q-header elevated class="bg-primary text-white text-left">
+      <q-toolbar>
+        <q-toolbar-title>
+          Shopicorn
+        </q-toolbar-title>
+      </q-toolbar>
+    </q-header>
+
+    <q-page-container>
+      <router-view />
+    </q-page-container>
+  </q-layout>
+</template>
+
+<script setup lang="ts">
+</script>
